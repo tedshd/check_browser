dc83e2c2a102df588cfa3fdbc465b3bbf9954231
Ted <tedshd@gmail.com>
2015-08-13 16:47:22 +0800

    [Fixed] Fixed code check.

--- browser.php
+++ browser.php
@@ -9,21 +9,6 @@
     function browser($value='')
     {
         $agent = $_SERVER['HTTP_USER_AGENT'];
-        if (strpos($agent, 'OPR') > 0) {
-            return 'Opera';
-        }
-        if (strpos($agent, 'Opera') > 0) {
-            return 'Opera';
-        }
-        if (strpos($agent, 'Firefox') > 0) {
-            return 'Firefox';
-        }
-        if (strpos($agent, 'Chrome') > 0) {
-            return 'Chrome';
-        }
-        if (strpos($agent, 'Safari') > 0) {
-            return 'Safari';
-        }
         if (strpos($agent, 'MSIE 7') > 0) {
             return 'IE7';
         }
@@ -44,6 +29,21 @@
         }
         if (strpos($agent, 'MSIE') > 0) {
             return 'IE';
+        }
+        if (strpos($agent, 'OPR') > 0) {
+            return 'Opera';
+        }
+        if (strpos($agent, 'Opera') > 0) {
+            return 'Opera';
+        }
+        if (strpos($agent, 'Firefox') > 0) {
+            return 'Firefox';
+        }
+        if (strpos($agent, 'Chrome') > 0) {
+            return 'Chrome';
+        }
+        if (strpos($agent, 'Safari') > 0) {
+            return 'Safari';
         }
 
         return 'unknown browser';
