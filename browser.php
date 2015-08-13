dd9346b703f3a7e25299e9b0fb74daa197198840
Ted <tedshd@gmail.com>
2015-08-13 16:57:34 +0800

    [

--- browser.php
+++ browser.php
@@ -9,6 +9,21 @@
     function browser($value='')
     {
         $agent = $_SERVER['HTTP_USER_AGENT'];
+        if (strpos($agent, 'OPR') > 0) {
+            return 'Opera';
+        }
+        if (strpos($agent, 'Opera') > 0) {
+            return 'Opera';
+        }
+        if (strpos($agent, 'Firefox') > 0) {
+            return 'Firefox';
+        }
+        if (strpos($agent, 'Chrome') > 0) {
+            return 'Chrome';
+        }
+        if (strpos($agent, 'Safari') > 0) {
+            return 'Safari';
+        }
         if (strpos($agent, 'MSIE 7') > 0) {
             return 'IE7';
         }
@@ -29,21 +44,6 @@
         }
         if (strpos($agent, 'MSIE') > 0) {
             return 'IE';
-        }
-        if (strpos($agent, 'OPR') > 0) {
-            return 'Opera';
-        }
-        if (strpos($agent, 'Opera') > 0) {
-            return 'Opera';
-        }
-        if (strpos($agent, 'Firefox') > 0) {
-            return 'Firefox';
-        }
-        if (strpos($agent, 'Chrome') > 0) {
-            return 'Chrome';
-        }
-        if (strpos($agent, 'Safari') > 0) {
-            return 'Safari';
         }
 
         return 'unknown browser';
